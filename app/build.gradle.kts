85b930ab4a5e0b42f8adbac6e5208fa7ca7d7607
Nima <nimahakim.ad@gmail.com>
2024-05-05 20:04:13 +0330

    add Application class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-common-java8:2.7.0")
+
+    implementation("androidx.activity:activity-ktx:1.9.0")
 }
