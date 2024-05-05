2b01427d89c049a05dfdded11c32b9f01384e7d9
Nima <nimahakim.ad@gmail.com>
2024-05-05 18:47:33 +0330

    add ui

--- app/src/main/java/com/example/smsmanage/MainActivity.kt
+++ app/src/main/java/com/example/smsmanage/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.smsmanage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smsmanage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
